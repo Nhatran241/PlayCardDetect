72b4c6839defe185bae3a9cc818968af19be6ac0
Trần Minh Nhật <nhat.tran@youthdev.net>
2020-08-22 14:03:29 +0700

    change to kotlin

---
+++ app/src/main/java/com/machinelearning/playcarddetect/modules/admin/business/AdminActivityViewModel.kt
@@ -0,0 +1,16 @@
+package com.machinelearning.playcarddetect.modules.admin.business
+
+class AdminActivityViewModel{
+    var listColor =
+        listOf(
+            ItemColorViewModel(bgColor = Color.BLACK),
+            ItemColorViewModel(bgColor = Color.BLUE),
+            ItemColorViewModel(bgColor = Color.CYAN),
+            ItemColorViewModel(bgColor = Color.DKGRAY),
+            ItemColorViewModel(bgColor = Color.GRAY),
+            ItemColorViewModel(bgColor = Color.LTGRAY),
+            ItemColorViewModel(bgColor = Color.MAGENTA),
+            ItemColorViewModel(bgColor = Color.GREEN),
+            ItemColorViewModel(bgColor = Color.YELLOW))
+
+}
---
+++ app/src/main/java/com/machinelearning/playcarddetect/modules/admin/business/ItemCardViewModel.kt
@@ -0,0 +1,9 @@
+package com.machinelearning.playcarddetect.modules.admin.business
+
+import android.graphics.RectF
+import com.machinelearning.playcarddetect.common.model.CardBase64
+
+class ItemListCardViewModel (
+    var base64: CardBase64,
+    var rectF: RectF
+)
